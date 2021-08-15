b7503d76d421534d3e79125a7f524ff0894e2af6
leikang123 <42248685+leikang123@users.noreply.github.com>
2021-08-15 21:46:11 +0800

    Update main.rs

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,13 +1,14 @@
+// 定义结构体
 trait doubt {
     fn dou(&self) -> Self;
 }
-
+// 实现结构体
 impl doubt for i32 {
     fn dou(&self) -> Self {
         *self * 2
     }
 }
-
+// 主函数
 fn main() {
     let x: i32 = 10.dou();
     print!("{}", x)
