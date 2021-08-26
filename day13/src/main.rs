1f3fe342527a28c7a17d4e858b6b8b8b0da10f69
leikang123 <42248685+leikang123@users.noreply.github.com>
2021-08-26 10:47:59 +0800

    Update main.rs

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
+    // 结构体颜色（三个参数）
     struct Color(u8, u8, u8);
+    // 结构体点（坐标）
     struct Point(f64, f64);
-
+    // 定义black 的颜色
     let black = Color(0, 0, 0);
     let origin = Point(0.0, 0.0);
 
